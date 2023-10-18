4bdac39ca80c5034003402e230305dc1e457f201
AiHikki <freeomareps@gmail.com>
2023-10-18 21:54:29 +0300

    Update task-04.js

--- task-04.js
+++ task-04.js
@@ -6,7 +6,7 @@ function getShippingCost(country) {
     case 'Chile':
       return `Shipping to ${country} will cost 250 credits`;
     case 'Australia':
-      return `Shipping to ${country} will cost 70 credits`;
+      return `Shipping to ${country} will cost 170 credits`;
     case 'Jamaica':
       return `Shipping to ${country} will cost 120 credits`;
     default:
